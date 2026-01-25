b1ae4228bd9a3af85bff9c3458b255aef2f599fe
HannanMaxwell <__max__@live.com>
2026-01-25 16:44:44 -0600

    Created the api endpoint for the portfolio performance and the test for it.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import { calculatePortfolioPerformance } from "./portfolio/portfolioPerformance";
 
 let app: Express = express();
 
@@ -10,5 +11,16 @@ app.get("/api/v1/health", (req, res) => {
         version: "1.0.0",
     });
 });
+
+app.get("/api/v1/portfolio/performance", (req, res) => {
+    const initialInvestment = parseFloat(req.query.initialInvestment as string);
+    const currentValue = parseFloat(req.query.currentValue as string);
+
+    const result = calculatePortfolioPerformance(initialInvestment, currentValue);
+    res.json(result);
+    
+});
+
+
 
 export default app;
---
+++ test/server.test.ts
@@ -0,0 +1,40 @@
+import request, { Response } from "supertest";
+import app from "../src/app";
+
+// test for health api endpoint
+
+describe("GET /api/v1/health", () => {
+    it("should return server health status", async () => {
+        const response: Response = await request(app).get("/api/v1/health");
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("OK");
+        expect(response.body).toHaveProperty("uptime");
+        expect(response.body).toHaveProperty("timestamp");
+        expect(response.body).toHaveProperty("version");
+    });
+});
+
+
+
+// test for portfolio API Endpoint
+
+describe("GET /api/v1/portfolio/performance", () => {
+    it("should calculate portfolio performance and return 200 with correct data", async () => {
+        // Arrange
+        const initialInvestment = 10000;
+        const currentValue = 12000;
+        
+        // Act
+        const response = await request(app).get("/api/v1/portfolio/performance").query({ initialInvestment, currentValue });
+        
+        // Assert
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            initialInvestment: 10000,
+            currentValue: 12000,
+            profitOrLoss: 2000,
+            percentageChange: 20,
+            performanceSummary: expect.stringContaining("Solid gain")
+        });
+    });
+});
